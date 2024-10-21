a4fac7d3e3f6ef25d492582b6bb720f4f7b4508d
Satwik Mishra <150122188+satwikmishra11@users.noreply.github.com>
2024-10-21 20:45:40 +0530

    Update main.dart
    adding custom boards for math and gujrati

--- main.dart
+++ main.dart
@@ -1,3 +1,84 @@
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gujarati Learning App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gujarati Learning App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AlphabetScreen()),
+                );
+              },
+              child: Text('Learn Alphabets'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MathScreen()),
+                );
+              },
+              child: Text('Learn Mathematics'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AlphabetScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gujarati Alphabets'),
+      ),
+      body: Center(
+        child: Text('Alphabet learning goes here...'),
+      ),
+    );
+  }
+}
+
+class MathScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mathematics'),
+      ),
+      body: Center(
+        child: Text('Math learning goes here...'),
+      ),
+    );
+  }
+}
+
 import 'package:flutter/material.dart';
 import 'package:signature/signature.dart';
 
