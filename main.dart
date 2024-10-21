04ce7f1e11f2a64de08a200d306a8b4d8c10a13c
Satwik Mishra <150122188+satwikmishra11@users.noreply.github.com>
2024-10-21 20:47:18 +0530

    Update main.dart
    added feature of speech to text

--- main.dart
+++ main.dart
@@ -189,4 +189,51 @@ class _SignLanguageScreenState extends State<SignLanguageScreen> {
     );
   }
 }
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+
+class SpeechToSignLanguage extends StatefulWidget {
+  @override
+  _SpeechToSignLanguageState createState() => _SpeechToSignLanguageState();
+}
+
+class _SpeechToSignLanguageState extends State<SpeechToSignLanguage> {
+  stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _text = "Press the button and start speaking";
+
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
+
+  void _listen() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize();
+      if (available) {
+        setState(() => _isListening = true);
+        _speech.listen(onResult: (val) => setState(() {
+              _text = val.recognizedWords;
+              // Convert the recognized text to sign language (play video)
+            }));
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Speech to Sign Language")),
+      body: Center(child: Text(_text)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _listen,
+        child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+      ),
+    );
+  }
+}
 
